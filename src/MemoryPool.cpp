ea42eb99662a5213cd955f3a5972f22d9bfdda2e
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-03-01 15:56:33 -0700

    src/MemoryPool: reserve -> resize

--- src/MemoryPool.cpp
+++ src/MemoryPool.cpp
@@ -5,7 +5,7 @@
 MemoryPool::MemoryPool(size_t block_size, size_t num_blocks)
     : m_block_size{block_size}, m_num_blocks{num_blocks}
 {
-    m_data.reserve(m_block_size * m_num_blocks);
+    m_data.resize(m_block_size * m_num_blocks);
 
     for (size_t i = 0; i < m_num_blocks; ++i) {
       m_freelist.push_back(&m_data[i*m_block_size]);
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,13 @@ int main() {
   MemoryPool pool {sizeof(int), 10};
 
   int* p1 = static_cast<int*>(pool.allocate());
-  std::cout << "p1 allocated to: " << *p1 << std::endl;
-  *p1 = 42;
-  std::cout << "p1 allocated to: " << *p1 << std::endl;
-
   int* p2 = static_cast<int*>(pool.allocate());
-  std::cout << "p2 allocated to: " << *p2 << std::endl;
-  *p2 = 108;
-  std::cout << "p2 allocated to: " << *p2 << std::endl;
+
+  *p1 = 50;
+  *p2 = 100;
+
+  std::cout << *p1 << std::endl;
+  std::cout << *p2 << std::endl;
 
   pool.deallocate(p1);
   pool.deallocate(p2);
